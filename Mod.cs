9a92b8ff7529c5e9f2584a6890d4efdf255c333f
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2024-01-08 13:59:29 +1100

    Updated to 1.0.1
    - Fixed mod not working...
    - Removed god mod cheat that was used to develop

--- Mod.cs
+++ Mod.cs
@@ -1,104 +1,34 @@
 ﻿using BepInEx;
-using GameNetcodeStuff;
 using HarmonyLib;
-using System;
-using System.IO;
-using UnityEngine;
 
 namespace LethalJojo
 {
-    // TODO: Make Bundle load from local path not streaming assets
-    // TODO: Make mask use model
     // TODO: Turn off red eyes
 
-    [BepInPlugin(ID, NAME, VERSION)]
+    public static class ModInfo
+    {
+        public const string NAME = "Lethal Jojo";
+        public const string ID = "dev.panda.lethaljojo";
+        public const string DESCRIPTION = "Have a little slice of Jojo in your game";
+        public const string COMPANY = "Pandas Hell Hole";
+        public const string URL = "https://github.com/LeCloutPanda/LethalJojo";
+        public const string AUTHOR = "LeCloutPanda";
+        public const string VERSION = "1.0.1";
+    }
+
+    [BepInPlugin(ModInfo.ID, ModInfo.NAME, ModInfo.VERSION)]
     public class Mod : BaseUnityPlugin
     {
-        private const string ID = "dev.panda.lethaljojo";
-        private const string NAME = "Lethal Jojo";
-        private const string VERSION = "1.0.0";
-
-        private static Mesh _stoneMaskMesh = null;
+        private static StoneMaskPatch StoneMaskPatch = null;
 
         void Awake()
         {
-            Logger.LogMessage($"Loaded {NAME} v{VERSION}");
-            Harmony harmony = new Harmony("dev.panda.lethaljojo");
+            Logger.LogMessage($"Loaded {ModInfo.NAME} v{ModInfo.VERSION}");
+
+            StoneMaskPatch = new StoneMaskPatch();
+
+            Harmony harmony = new Harmony(ModInfo.ID);
             harmony.PatchAll();
-
-            try
-            {
-
-                var asssetBundle = AssetBundle.LoadFromFile(Path.Combine(Paths.PluginPath, "LethalJojo/LethalJojoAssetBundle"));
-
-                if (asssetBundle != null)
-                {
-                    _stoneMaskMesh = asssetBundle.LoadAsset<Mesh>("StoneMask_Model");
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e);
-            }
-        }
-
-        [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.Start))]
-        private static class ChangeMaskToStoneMask
-        {
-            [HarmonyPostfix]
-            public static void Postfix(GrabbableObject __instance)
-            {
-                __instance.TryGetComponent<HauntedMaskItem>(out HauntedMaskItem mask);
-                if (mask == null) return;
-
-                __instance.transform.FindChild("MaskMesh").TryGetComponent<MeshFilter>(out MeshFilter main);
-                if (main != null) main.mesh = _stoneMaskMesh;
-
-                __instance.transform.FindChild("MaskMesh").FindChild("ComedyMaskLOD1").TryGetComponent<MeshFilter>(out MeshFilter LOD1);
-                if (LOD1 != null) LOD1.mesh = _stoneMaskMesh;
-            }
-        }
-
-        [HarmonyPatch(typeof(HauntedMaskItem), nameof(HauntedMaskItem.MaskClampToHeadAnimationEvent))]
-        private static class ChangeMaskPrefabModel
-        {
-            [HarmonyPostfix]
-            public static void Postfix(HauntedMaskItem __instance, Transform ___currentHeadMask)
-            {
-                ___currentHeadMask.FindChild("ComedyMaskLOD1").TryGetComponent<MeshFilter>(out MeshFilter LOD1);
-                if (LOD1 != null) LOD1.mesh = _stoneMaskMesh;
-
-                ___currentHeadMask.FindChild("Mesh").TryGetComponent<MeshFilter>(out MeshFilter Mesh);
-                if (Mesh != null) Mesh.mesh = _stoneMaskMesh;
-            }
-        }  
-        
-        // Works
-        [HarmonyPatch(typeof(MaskedPlayerEnemy), "Awake")]
-        private static class ChangeEntityMasks
-        {
-            [HarmonyPrefix]
-            public static void Prefix(MaskedPlayerEnemy __instance)
-            {
-                foreach(GameObject obj in __instance.maskTypes)
-                {
-                    obj.transform.FindChild("ComedyMaskLOD1").TryGetComponent<MeshFilter>(out MeshFilter LOD1);
-                    if (LOD1 != null) LOD1.mesh = _stoneMaskMesh;
-
-                    obj.transform.FindChild("Mesh").TryGetComponent<MeshFilter>(out MeshFilter Mesh);
-                    if (Mesh != null) Mesh.mesh = _stoneMaskMesh;
-                }
-            }
-        }
-
-        [HarmonyPatch(typeof(PlayerControllerB), "KillPlayer")]
-        private static class GodMode
-        {
-            [HarmonyPrefix]
-            public static bool Prefix(PlayerControllerB __instance)
-            {
-                return false;
-            }
         }
     }
 }
---
+++ StoneMaskPatch.cs
@@ -0,0 +1,90 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+using System;
+using UnityEngine;
+
+namespace LethalJojo
+{
+    public class StoneMaskPatch : MonoBehaviour
+    {
+        private static Mesh _stoneMaskMesh = null;
+
+        public StoneMaskPatch() 
+        {
+            Console.WriteLine("It isn't done.");
+
+            try
+            {
+                var asssetBundle = AssetBundle.LoadFromFile(Path.Combine(Paths.PluginPath, "PandasHellCrew-LethalJojo/LethalJojoAssetBundle"));
+
+                if (asssetBundle != null)
+                {
+                    _stoneMaskMesh = asssetBundle.LoadAsset<Mesh>("StoneMask_Model");
+                    Console.WriteLine("Loaded mask from bundle.");
+                }
+                else 
+                {
+                    Console.WriteLine("Failed loading mask you numpty!");
+                }
+            }
+            catch (UnityException e)
+            {
+                Console.WriteLine("It fucking died lmao ;)");
+                Console.WriteLine(e.StackTrace);
+            }
+
+            Console.WriteLine("It is done.");
+        }
+
+        [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.Start))]
+        private static class ChangeMaskToStoneMask
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GrabbableObject __instance)
+            {
+                __instance.TryGetComponent<HauntedMaskItem>(out HauntedMaskItem mask);
+                if (mask == null) return;
+
+                __instance.transform.FindChild("MaskMesh").TryGetComponent<MeshFilter>(out MeshFilter main);
+                if (main != null) main.mesh = _stoneMaskMesh;
+
+                __instance.transform.FindChild("MaskMesh").FindChild("ComedyMaskLOD1").TryGetComponent<MeshFilter>(out MeshFilter LOD1);
+                if (LOD1 != null) LOD1.mesh = _stoneMaskMesh;
+            }
+        }
+
+        [HarmonyPatch(typeof(HauntedMaskItem), nameof(HauntedMaskItem.MaskClampToHeadAnimationEvent))]
+        private static class ChangeMaskPrefabModel
+        {
+            [HarmonyPostfix]
+            public static void Postfix(HauntedMaskItem __instance, Transform ___currentHeadMask)
+            {
+                ___currentHeadMask.FindChild("ComedyMaskLOD1").TryGetComponent<MeshFilter>(out MeshFilter LOD1);
+                if (LOD1 != null) LOD1.mesh = _stoneMaskMesh;
+
+                ___currentHeadMask.FindChild("Mesh").TryGetComponent<MeshFilter>(out MeshFilter Mesh);
+                if (Mesh != null) Mesh.mesh = _stoneMaskMesh;
+            }
+        }
+
+        // Works
+        [HarmonyPatch(typeof(MaskedPlayerEnemy), "Awake")]
+        private static class ChangeEntityMasks
+        {
+            [HarmonyPrefix]
+            public static void Prefix(MaskedPlayerEnemy __instance)
+            {
+                foreach (GameObject obj in __instance.maskTypes)
+                {
+                    obj.transform.FindChild("ComedyMaskLOD1").TryGetComponent<MeshFilter>(out MeshFilter LOD1);
+                    if (LOD1 != null) LOD1.mesh = _stoneMaskMesh;
+
+                    obj.transform.FindChild("Mesh").TryGetComponent<MeshFilter>(out MeshFilter Mesh);
+                    if (Mesh != null) Mesh.mesh = _stoneMaskMesh;
+                }
+            }
+        }
+
+    }
+}
